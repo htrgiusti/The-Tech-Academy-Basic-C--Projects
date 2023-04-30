cbac1ffab3d23de5af112d1f54ef28966acf6e00
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-01 00:51:59 +0100

    Delete Branching Assignment.cs
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

--- Branching Assignment.cs
+++
@@ -1,3 +0,0 @@
-﻿'Branching Assignment.exe' (CLR v4.0.30319: DefaultDomain): Loaded 'C:\Windows\Microsoft.Net\assembly\GAC_32\mscorlib\v4.0_4.0.0.0__b77a5c561934e089\mscorlib.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'Branching Assignment.exe' (CLR v4.0.30319: DefaultDomain): Loaded 'C:\Users\Administrator\Desktop\Branching Assignment\Branching Assignment\Branching Assignment\bin\Debug\Branching Assignment.exe'. Symbols loaded.
-The program '[16652] Branching Assignment.exe' has exited with code -1073741510 (0xc000013a).
