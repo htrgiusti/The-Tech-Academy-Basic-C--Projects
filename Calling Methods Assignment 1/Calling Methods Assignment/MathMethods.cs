dcad95652563a11f09196c5731b47e05544678ea
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-11 03:12:34 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Calling Methods Assignment 1/Calling Methods Assignment/MathMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class MathMethods
+    {
+        // Create three methods that take one integer parameter and return an integer 
+        public static int Heal (int magic)
+        {
+            int hitpoints = magic + 10;
+            return hitpoints;
+        }
+        public static int Defense(int defense)
+        {
+            int enemy_str = 7;
+            int weapon_mt = 11;
+            int triangle_bonus = -1;
+            int crit_coeff = 1;
+            int damage = (enemy_str + (weapon_mt + triangle_bonus) - defense) * crit_coeff;
+            return damage;
+        }
+        public static int Attack(int strenght)
+        {
+            int enemy_def = 4;
+            int weapon_mt = 8;
+            int triangle_bonus = 1;
+            int crit_coeff = 3;
+            int damage = (strenght + (weapon_mt + triangle_bonus) - enemy_def) * crit_coeff;
+            return damage;
+        }
+    }
+}
