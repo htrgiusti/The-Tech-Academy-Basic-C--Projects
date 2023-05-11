052a8e6b0492bc881dbdbc4e6eca9f732874ce3a
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-11 05:12:12 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Calling Methods Assignment/Calling Methods Assignment 4/Calling Methods Assignment 4/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment_4
+{
+    class MathOperation
+    {
+        // Create a method that takes two integers as parameters
+        // Make one parameter optional by providing a default value
+        public int MathOp(int i, int x = 1)
+        {
+            // Perform multiplication on integers and return the result 
+            int result = i * x;
+            return result;
+        }
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment 4/Calling Methods Assignment 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate MathOperation class
+            MathOperation math1 = new MathOperation();
+            // Ask the user to provide first parameter
+            Console.WriteLine("Enter an integer number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            // Ask the user to provide a second parameter or press enter
+            Console.WriteLine("Enter a second integer or press enter.");
+            try
+            {
+                // Call method with 2 parameters if both parameters are provided
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = math1.MathOp(num1, num2);
+                Console.WriteLine(num1 + " x " + num2 + " = " + results);
+            }
+            catch
+            {
+                // Call method with one parameter if only one is provided 
+                int results = math1.MathOp(num1);
+                Console.WriteLine(num1 + " x Default 1" + " = " + results);
+            }              
+            Console.Read();
+        }
+    }
+}
