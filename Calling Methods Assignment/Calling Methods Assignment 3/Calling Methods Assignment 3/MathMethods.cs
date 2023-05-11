01b7de17b5591939b31575fc9888369a2a831c46
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-11 04:38:58 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Calling Methods Assignment/Calling Methods Assignment 3/Calling Methods Assignment 3/MathMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment_3
+{
+    class MathMethods
+    {
+        // First Method that takes an integer
+        public int MathOp(int number)
+        {
+            return number + 1000;
+        }
+        
+        // Second Method that takes a decimal 
+        public int MathOp(decimal number)
+        {
+            int newnum = Convert.ToInt32(number);
+            return newnum * 1000;
+        }
+        
+        // Third Method that takes a string
+        public int MathOp(string number)
+        {
+            int newnum = Convert.ToInt32(number);
+            return newnum / 4;
+        }
+        
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment 3/Calling Methods Assignment 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathMethods class
+            MathMethods math1 = new MathMethods();
+
+            // Call the first method with an int parameter 
+            Console.WriteLine(math1.MathOp(10));
+
+            // Call the second method with a decimal parameter 
+            Console.WriteLine(math1.MathOp(5.5m));
+
+            // Call the third method with a string parameter 
+            Console.WriteLine(math1.MathOp("1000"));
+            
+
+            Console.Read();
+        }
+    }
+}
