379cf7a5e6129b6ca272867751230529e12d8176
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-11 03:44:48 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Calling Methods Assignment/Calling Methods Assignment 2/Calling Methods Assignment 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Ask the user for input
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());          
+
+            // Call each method and display the returned integer 
+            int squaredNumber = MathOperations.SquareNumber(userInput);
+            Console.WriteLine("Square of " + userInput + " is: " + squaredNumber);
+
+            int sum = MathOperations.CalculateSum(userInput);
+            Console.WriteLine("Sum of numbers up to " + userInput + " is: " + sum);
+
+            Console.Read();
+        }
+    }
+}
