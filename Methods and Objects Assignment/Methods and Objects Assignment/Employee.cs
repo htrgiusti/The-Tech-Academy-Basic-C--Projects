e50a4da0dea88cc95fdd00ff1db17ffca00aa49a
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-15 03:01:08 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Methods and Objects Assignment/Methods and Objects Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    // Create another class called Employee and have it inherit from the Person class.
+    // Give the Employee class a property called Id and have it be of data type int.
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Methods and Objects Assignment/Methods and Objects Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+            // Call the superclass method SayName() on the Employee object.
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
