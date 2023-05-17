c820fc10f447346af0e642350ff5b537d0c1b17b
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-17 09:32:11 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Method Class Assignment/Method Class Assignment/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class MathOperations
+    {
+        // Create a class. In that class, create a void method that takes two integers as parameters.
+        // Have the method do a math operation on the first integer and display the second integer to the screen. 
+        public void method(int numberOne, int numberTwo)
+        {
+            int result = numberOne * 2;
+            Console.WriteLine("Number one + 2 = " + result);
+
+            Console.WriteLine("Number two = " + numberTwo);
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // In the Main() method of the console app, instantiate the class.
+            MathOperations mathops = new MathOperations();
+
+            // Call the method in the class passing in two numbers and specifying the parameters by name.
+            mathops.method(numberOne: 10, numberTwo: 21);
+            
+            Console.Read();
+        }
+    }
+}
