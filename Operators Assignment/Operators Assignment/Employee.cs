3813f73bd454b5f4e017d7d0fd3e3b2d3d0c18be
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-23 16:57:07 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Employee
+    {
+        // Create an Employee class with Id, FirstName and LastName properties. 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // In the Employee class, overload the “==” operator so it checks if two Employee
+        // objects are equal by comparing their Id property.
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1?.Id == employee2?.Id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1?.Id != employee2?.Id;
+        }
+
+
+    }
+}
---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating Employee objects
+            Employee emp1 = new Employee { Id = 1, FirstName = "Renato", LastName = "Onix" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Juliana", LastName = "Novac" };
+            Employee emp3 = new Employee { Id = 1, FirstName = "Heitor", LastName = "Giusti" };
+
+
+            // Performing comparisons using overloaded operators
+            Console.WriteLine($"Employee 1 \n Id: {emp1.Id} \n First Name: {emp1.FirstName} \n Last Name: {emp1.LastName}\n");
+            Console.WriteLine($"Employee 2 \n Id: {emp2.Id} \n First Name: {emp2.FirstName} \n Last Name: {emp2.LastName}\n");
+            Console.WriteLine($"Employee 3 \n Id: {emp3.Id} \n First Name: {emp3.FirstName} \n Last Name: {emp3.LastName}\n");
+
+            Console.WriteLine($"Employee 1 Id == Employee 2 Id: {emp1 == emp2}");
+            Console.WriteLine($"Employee 1 Id == Employee 3 Id: {emp1 == emp3}");
+            
+            Console.WriteLine($"Employee 1 Id != Employee 2 Id: {emp1 != emp2}");
+            Console.WriteLine($"Employee 1 Id != Employee 3 Id: {emp1 != emp3}");
+            
+
+
+
+            Console.Read();
+
+        }
+    }
+}
