6800255860ef947787c045f8db7e9d091e03adf6
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-23 19:08:43 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Parsing Enums Assignment/Parsing Enums Assignment/DaysOfWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment
+{
+    // Create an enum for the days of the week.
+    enum DaysOfWeek
+    {
+       
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    
+    }
+}
---
+++ Parsing Enums Assignment/Parsing Enums Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Prompt the user to enter the current day of the week.
+                Console.WriteLine("What day is today?");
+                string userInput = Console.ReadLine();
+                
+                // Assign the value to a variable of that enum data type you just created.
+                DaysOfWeek currentDay;
+                if (Enum.TryParse(userInput, true, out currentDay))
+                {
+                    Console.WriteLine($"Today is: {currentDay}");
+                }
+                
+                // Wrap the above statement in a try/catch block and have it print
+                // "Please enter an actual day of the week.” to the console if an error occurs.
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error has occurred: {ex.Message}");
+            }
+            Console.Read();
+        }
+    }
+}
