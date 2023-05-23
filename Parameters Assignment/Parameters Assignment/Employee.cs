ee82a5fbcb626e11abc8a5d315a3ef8836a435b9
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-23 18:17:28 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Parameters Assignment/Parameters Assignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    // Make the Employee class take a generic type parameter.
+    class Employee<T>
+    {
+        // Add a property to the Employee class called “things” and have its data type be
+        // a generic list matching the generic type of the class.
+        public List<T> Things = new List<T>();
+
+        public Employee()
+        {
+            Things = new List<T>();
+        }
+
+    }
+}
---
+++ Parameters Assignment/Parameters Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with type “string” as its generic parameter.
+            // Assign a list of strings as the property value of Things.
+            Employee<string> employeeString = new Employee<string>();
+            employeeString.Things = new List<string> { "Bear", "Boar", "Spider" };
+
+            // Instantiate an Employee object with type “int” as its generic parameter.
+            // Assign a list of integers as the property value of Things.
+            Employee<int> employeeInt = new Employee<int>();
+            employeeInt.Things = new List<int> { 1, 2, 3 };
+
+            // Create a loop that prints all of the Things to the Console.
+            Console.WriteLine("\n Employee string list: ");
+            foreach (string thing in employeeString.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\n Employee int list: ");
+            foreach (int thing in employeeInt.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.Read();
+
+
+
+        }
+    }
+}
