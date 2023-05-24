d4e5ac81515fbd8f54370777aa33de30fdfe5ae5
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-05-25 00:13:18 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Struct Assignment/Struct Assignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    // Create a struct called Number and give it the property “Amount” and have it be of data type decimal.
+    struct Number
+    {
+        public decimal Amount;
+    }
+}
---
+++ Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the Main() method, create an object of data type Number and assign an amount to it.
+            Number num = new Number();
+            num.Amount = 9.99m;
+
+            // Print this amount to the console.
+            Console.WriteLine(num.Amount);
+
+            Console.Read();
+
+        }
+    }
+}
