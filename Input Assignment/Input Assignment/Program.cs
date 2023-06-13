a8d761d829258bed39b10cd6bfc3feb55c287954
Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>
2023-06-14 00:13:03 +0100

    Add files via upload
    Signed-off-by: Heitor Giusti <99479645+htrgiusti@users.noreply.github.com>

---
+++ Input Assignment/Input Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number: "); // Asks the user to type a number
+            string userInput = Console.ReadLine(); // Stores the user's input in the variable "userInput"
+            File.WriteAllText(@"C:\Users\Heitor\Logs\log.txt", userInput); // Creates a log.txt and writes the contents of "userInput" into it
+            Console.WriteLine("Log print: "); // Prints the line "Log print: "
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Heitor\Logs\log.txt")); // Reads the contents of the "log.txt" file, which contains the user's input, and prints it
+
+            Console.Read();
+            
+        }
+    }
+}
